994e7f04fa0378276f58f301e817ee6151105f24
LIEU Thierry <thierry.lieu@sap.com>
2019-01-23 10:22:38 +0000

    Implementation of Step 8: Translatable Texts

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -2,7 +2,8 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/m/MessageToast",
 	"sap/ui/model/json/JSONModel",
-	], function(Controller,MessageToast,JSONModel){
+	"sap/ui/model/resource/ResourceModel"	
+	], function(Controller,MessageToast,JSONModel,ResourceModel){
 		"use strict";
 // The "use strict"; literal expression was introduced by JavaScript 1.8.5 
 // (ECMAScript 5). It tells the browser to execute the code in a so called 
@@ -20,10 +21,22 @@ sap.ui.define([
 			}; 
 			var oModel = new JSONModel(oData);
 			this.getView().setModel(oModel);
+			
+			// set i18n model on view
+			var i18nModel = new ResourceModel({
+				bundleName:"sap.ui.demo.walkthrough.i18n.i18n"
+			});
+			this.getView().setModel(i18nModel,"i18n");
 			},
 			
 			onShowHello: function() {
-				MessageToast.show("Hello World");
+				// read msg from i18n model
+				var oBundle = this.getView().getModel("i18n").getResourceBundle();
+				var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+				var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+				
+				// show message
+				MessageToast.show(sMsg);
 			}
 		});
 	});
